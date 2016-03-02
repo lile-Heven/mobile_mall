b0679cf2f0d76e09fe159e125335da5110ab90c7
huayu.lile <315059124@qq.com>
2016-03-02 19:17:18 +0800

    仿京东手机商城版本0.2

---
+++ com.huayu.lile.mobile_mall0.2/gen/com/example/com/huayu/lile/mobile_mall/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.com.huayu.lile.mobile_mall;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ com.huayu.lile.mobile_mall0.2/src/com/huayu_lile/mobile_mall/fragment/MyFragment_FL.java
@@ -0,0 +1,18 @@
+package com.huayu_lile.mobile_mall.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.com.huayu.lile.mobile_mall.R;
+
+public class MyFragment_FL extends Fragment{
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    		Bundle savedInstanceState) {
+    	// TODO 自动生成的方法存根
+    	return inflater.inflate(R.layout.fragment_fl, null, false);
+    }
+}
